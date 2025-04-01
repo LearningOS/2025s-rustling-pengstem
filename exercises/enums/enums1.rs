9561c3a98adc37b5a871af30fb8e96dc62ffed14
pengsteam <pengsteam16@gmail.com>
2025-04-01 22:08:03 +0800

    Implement enum variants and struct definitions for exercises

--- exercises/enums/enums1.rs
+++ exercises/enums/enums1.rs
@@ -2,12 +2,8 @@
 //
 // No hints this time! ;)
 
-// I AM NOT DONE
-
 #[derive(Debug)]
-enum Message {
-    // TODO: define a few types of messages as used below
-}
+enum Message {Quit,Echo,Move,ChangeColor}
 
 fn main() {
     println!("{:?}", Message::Quit);
